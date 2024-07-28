52d1574e1c3c2c168cd08b00766e8e2de67e7f94
shwetha <skulal386@gmail.com>
2024-07-28 19:10:54 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ function App() {
 
   return (
    <><div className=" h-[100vh] w-full flex justify-center items-center text-center">
-      
+      {/* <div className="bg-orange-500 w-full h-56 sm:bg-red-400 md:bg-green-700 lg:bg-cyan-400 xl:bg-violet-600">
+     <h1 className="text-white">Hai I'm shwetha</h1>
+     <div className="flex flex-col md:flex-row bg-amber-200">
+      <div className="text-black flex md:flex-row w-full justify-center items-center">Moodbidri</div>
+      <div className="text-black flex flex-col w-full justify-center items-center">Mangalore</div>
+     </div>
+    </div> */}
     <div >
       <h1 className="font-bold text-black" >{count}</h1>
       <button onClick={()=>setCount(count + 1)} className="font-bold text-black bg-sky-300 border border-black">Increase</button>
